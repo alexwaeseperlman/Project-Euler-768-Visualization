800d49dbe37d5fa83afd5f3e01751ccba2090512
Alex Waese-Perlman <alexwaeseperlman@gmail.com>
2022-11-24 17:54:28 -0500

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { CssVarsProvider } from "@mui/joy/styles";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <CssVarsProvider>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </CssVarsProvider>
+  );
 }
